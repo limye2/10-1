456865c62d0fe9ac14e85adf161d89f9aee5dc07
limye2 <144992260+limye2@users.noreply.github.com>
2023-11-12 22:32:59 +0900

    Initial Commit

---
+++ 10-1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  10-1
+//
+//  Created by MacBook Air on 2023/11/12.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, const char * argv[]) {
+    char code1 = 'A';
+    char code2 = 65;
+    
+    printf("code1 = %x, (%d)\n", code1, code1);
+    printf("code2 = %x, (%d)\n", code2, code2);
+    return 0;
+}
